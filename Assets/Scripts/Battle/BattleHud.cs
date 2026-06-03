02f22ad24526f8bfb1fc4c30a9c9a372c4bf27a2
Futlop <nickaposto2003@gmail.com>
2026-06-03 11:38:50 -0700

    Updated the health bar to change colour based on remaining HP

--- Assets/Scripts/Battle/BattleHud.cs
+++ Assets/Scripts/Battle/BattleHud.cs
@@ -8,11 +8,21 @@ public class BattleHud : MonoBehaviour
     [SerializeField] Text nameText;
     [SerializeField] Text levelText;
     [SerializeField] HPBar hpBar;
+    private float percent = 1;
 
     public void SetData(Creature creature)
     {
         nameText.text = creature.Base.Name;
         levelText.text = "Lv. " + creature.Level;
         hpBar.SetHP((float) creature.HP / creature.MaxHP);
+    }
+
+    public void Update()
+    {
+        if(percent >= 0f)
+        {
+            hpBar.SetHP(percent);
+            percent -= 0.001f;
+        }
     }
 }
--- Assets/Scripts/Battle/HPBar.cs
+++ Assets/Scripts/Battle/HPBar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HPBar : MonoBehaviour
 {
@@ -9,5 +10,15 @@ public class HPBar : MonoBehaviour
     public void SetHP(float HPNormalized)
     {
         health.transform.localScale = new Vector3(HPNormalized, 1f);
+
+        // Change health bar colour based on remaining HP.  Green at 100%, becomes gradually more yellow at 50%, becomes gradually more red at 0%
+        if(HPNormalized >= 0.5)
+        {
+            health.GetComponent<Image>().color = new Color(((HPNormalized-1)*-2), 1, 0);
+        }
+        else
+        {
+            health.GetComponent<Image>().color = new Color(1, ((HPNormalized)*2), 0);
+        }
     }
 }
